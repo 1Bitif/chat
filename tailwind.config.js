0b8be5d403c7e3b17ed65549d5a586cd143ffbae
1Bitif <aajliabdellatif@gmail.com>
2024-11-06 18:41:19 +0100

    change primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#4A90E2", // Light Blue
-        secondary: "#50E3C2", // Soft Green
+        primary: "#8000FF", // Light Blue
+        secondary: "#D1B3FF", // Soft Green
         background: "#F5F7FA", // Very Light Gray
         text: "#333333", // Dark Gray
         accent: "#FF6F61", // Coral
