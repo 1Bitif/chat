aa9b3e19ab415169b4aab85064cfa8a5998e0f4a
1Bitif <aajliabdellatif@gmail.com>
2024-11-11 23:06:12 +0100

    fixed register

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Sidebar = () => {
+  return (
+    <>
+        <div className="w-1/4 h-screen bg-primary">
+            <div className="">
+                qsd            
+            </div>
+        </div>
+    </>
+  )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,39 +1,10 @@
-import React, { useState } from 'react';
-import PhoneInput from "react-phone-input-2";
-import "react-phone-input-2/lib/style.css"; // Make sure to import the styles
-import { Typography, Button, Menu, MenuHandler, MenuList, MenuItem } from "@material-tailwind/react"; // Adjust imports as per your setup
+import React from 'react'
+import { Sidebar } from '../../components/Sidebar/Sidebar'
 
-const CODES = {
-  US: "+1",
-  CA: "+1",
-  // Add more countries and their codes here
-};
-
-const COUNTRIES = ["US", "CA"]; // Add more countries as needed
 export const Home = () => {
-    const [phone, setPhone] = useState("");
-    const [country, setCountry] = useState("US");
-    
-    return (
-        <div className="w-full mt-16 mx-16 flex justify-center">
-        {/* <Typography variant="small" color="blue-gray" className="mb-1 font-medium">
-          Enter Phone Number
-        </Typography> */}
-        <div className="relative flex w-full">
-         
-          <PhoneInput
-            country={country.toLowerCase()} 
-            value={phone}
-            onChange={setPhone} 
-            inputProps={{
-              name: "phone",
-              required: true,
-              autoFocus: true,
-            }}
-            inputClass="!border-t-blue-gray-200 placeholder:text-blue-gray-300 focus:!border-primary" // Adjust input styles
-          />
-        </div>
-      </div>
-    );
-};
-
+  return (
+    <>
+      <Sidebar />
+    </>
+  )
+}
