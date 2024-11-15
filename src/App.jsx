5435e2f02ea31caf2d71785d1b1fb0f575ac91ed
1Bitif <aajliabdellatif@gmail.com>
2024-11-15 22:56:10 +0100

    Added changes in sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { About } from './components/About/About';
 import { Login } from './pages/Login/Login';
 import { Register } from './pages/Register/Register';
 import { Home } from './pages/Home/Home';
 import { NoteFound } from './pages/NoteFound/NoteFound';
+import { Sidebar } from './components/Sidebar/Sidebar';
 
 export const App = () => {
   return (
@@ -12,10 +12,8 @@ export const App = () => {
       <Route path='/' element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-
-
-      {/* <Route path="/about" element={<About />} /> */}
       <Route path="/home" element={<Home />} />
+      <Route path="/side" element={<Sidebar />} />
       <Route path="*" element={<NoteFound />} />
     </Routes>
   );
