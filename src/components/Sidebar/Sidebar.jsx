066f6f5cc6845cafe086f8cf4f7693ace36fb1ee
1Bitif <aajliabdellatif@gmail.com>
2024-11-15 23:50:36 +0100

    changes sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -22,7 +22,7 @@ export const Sidebar = () => {
         </div>
         <div className='w-full flex flex-col mt-6 gap-8 justify-start items-center '>
           {sideTitle.map((item , index) => (
-            <div onClick={() => setClickSidebar(true)} className='w-full flex flow-row  gap-4 justify-between cursor-pointer' >
+            <div onClick={() => setClickSidebar(!clickSidebar)} className='w-full flex flow-row  gap-4 justify-between cursor-pointer' >
           <NavLink to={`#`} className={`flex items-center hover:bg-gray-300 gap-10 rounded-xl  w-11/12 p-4  ml-5  ${clickSidebar ? "bg-white text-primary" : ""}`}>
             {item.icon}
             <h1 className='text-xl font-medium'>{item.name}</h1>
