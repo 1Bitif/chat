59eb928951918648b88671d98934912f3b18e8e4
1Bitif <aajliabdellatif@gmail.com>
2024-11-16 15:04:57 +0100

    Added nex Components

---
+++ src/components/Chats/Chtas.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Chtas = () => {
+  return (
+    <div>Chtas</div>
+  )
+}
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,36 +4,20 @@ import { ChartNoAxesCombined, FolderClosed, LayoutGrid, Phone, Settings, Users }
 import { NavLink } from 'react-router-dom'
 
 export const Sidebar = () => {
-  const sideTitle = [
-    { name : "Dashboard" , icon : <LayoutGrid />},
-    { name : "Analytics" , icon : <ChartNoAxesCombined  />},
-    { name : "Files" , icon : <FolderClosed  />},  
-    { name : "Call" , icon : <Phone />},  
-    { name : "Community" , icon : <Users />},  
-    { name : "Settings" , icon : <Settings />},  
 
-  ]
-  const [clickSidebar , setClickSidebar] = useState(false)
   return (
     <>
-      <div className=' flex flex-col gap-6  items-center w-1/6 h-screen border-r border-primary ' >
-        <div className='p-6 border-b border-primary w-full flex justify-center'>
-            <TitleIcon width={""} height={""}  />
+      <div className=' flex   flex-col gap-6  items-center w-full h-screen ' >
+        <div className='p-6  w-full flex justify-center' >
+          <TitleIcon width={""} height={""} />
         </div>
-        <div className='w-full flex flex-col mt-6 gap-8 justify-start items-center '>
-          {sideTitle.map((item , index) => (
-            <div onClick={() => setClickSidebar(!clickSidebar)} className='w-full flex flow-row  gap-4 justify-between cursor-pointer' >
-          <NavLink to={`#`} className={`flex items-center hover:bg-gray-300 gap-10 rounded-xl  w-11/12 p-4  ml-5  ${clickSidebar ? "bg-white text-primary" : ""}`}>
-            {item.icon}
-            <h1 className='text-xl font-medium'>{item.name}</h1>
-          </NavLink>
-          <div className='bg-primary rounded-l-xl w-1/12'>
-          </div>
+        <div className='w-10/12 flex rounded-[30px] bg-primary  justify-center items-center '>
+            <div>
+              
             </div>
-          ))}
-         
-          
         </div>
+
+
 
       </div>
     </>
