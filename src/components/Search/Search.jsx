fdac49e506bfb25b392d6d3d896b9bc2fa6329c4
1Bitif <aajliabdellatif@gmail.com>
2024-11-16 20:17:06 +0100

    Added changes in Search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { SearchIcon, X } from 'lucide-react'
+import React, { useState } from 'react'
 
 export const Search = () => {
-  return (
-    <div>Search</div>
-  )
+    const [search, setSearch] = useState('');
+    const ClearSearch = () => {
+        setSearch('');
+    }
+    return (
+        <div className='w-full relative '>
+            <input 
+                onChange={(e) => setSearch(e.target.value)} 
+                value={search} type="text" placeholder='Search' 
+                className='w-full px-10 bg-gray-50 border-b  border-gray-300 text-gray-900 text-sm  focus:outline-none focus:border-primary block p-2.5' 
+            />
+
+            <div className='absolute top-2 left-2 text-gray-400'>
+                <SearchIcon />
+            </div>
+            { search && <div className='absolute top-2 right-2 text-gray-600 hover:text-primary cursor-pointer '>
+                <X size={20} className='' onClick={ClearSearch} />
+            </div>}
+        </div>
+    )
 }
