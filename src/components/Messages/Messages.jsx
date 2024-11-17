0e28fa21234945b46089e4246c4e951e4f080ad6
1Bitif <aajliabdellatif@gmail.com>
2024-11-17 16:47:14 +0100

    added new component

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Message } from '../Message/Message'
 
 export const Messages = () => {
   return (
-    <div>Messages</div>
+    <div>
+      <Message  />
+    </div>
   )
 }
