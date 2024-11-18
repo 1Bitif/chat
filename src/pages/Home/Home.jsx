45abacd55a310d7d7a4cbce73f49a57943c9d169
1Bitif <aajliabdellatif@gmail.com>
2024-11-18 21:42:33 +0100

    Fixed home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,28 +1,36 @@
-import React, { useState } from 'react'
-import { Sidebar } from '../../components/Sidebar/Sidebar'
-import { Chat } from '../../components/Chat/Chat'
-import { TitleIcon } from '../../icons/Icons'
+import React, { useState } from "react";
+import { Sidebar } from "../../components/Sidebar/Sidebar";
+import { Chat } from "../../components/Chat/Chat";
 
 export const Home = () => {
-  const [open, setOpen] = useState(false) 
-  const sideBar = () => {
-    // setOpen(setOpenSidebar)
+  const [open, setOpen] = useState(false);
 
-  }
-  // console.log("open", setOpenSidebar)
+  // const sideBar = () => {
+  //   setOpen(!open);
+  // };
+  console.log(open)
   return (
     <>
-      <div className='flex overflow-hidden justify-center items-center w-full h-screen bg-blue-50'>
-        <div className=' flex  shadow-xl bg-white overflow-hidden lg:w-[60%]  w-[100%] lg:h-[80%] h-full   rounded-xl'>
-          <div className={`lg:w-1/3 w-full lg:border-r ${open && "sm:block hidden w-full"}`}>
-            <Sidebar sideBar={sideBar} />
+      <div className="flex overflow-hidden justify-center items-center w-full h-screen bg-blue-50">
+        <div className="flex shadow-xl bg-white overflow-hidden lg:w-[60%] w-[100%] lg:h-[80%] h-full rounded-xl">
+          {/* Sidebar for both large and small screens */}
+          <div
+            className={`lg:w-1/3 w-full lg:border-r ${
+              open ? "hidden sm:block lg:block" : "block sm:block lg:block"
+            }`}
+          >
+            <Sidebar setOpen={setOpen} />
           </div>
-          <div className={`lg:w-2/3 ${!open && "sm:block hidden w-full"}` }>
-            <Chat setOpenSidebar={"setOpenSidebar"} />
+          {/* Chat for both large and small screens */}
+          <div
+            className={`lg:w-2/3 w-full ${
+              open ? "block sm:block lg:block" : "hidden sm:hidden lg:block"
+            }`}
+          >
+            <Chat setOpen={setOpen} />
           </div>
         </div>
       </div>
-
     </>
-  )
-}
+  );
+};
