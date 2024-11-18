16b19cc8e92bfed862627cb047d5f429499d55a4
1Bitif <aajliabdellatif@gmail.com>
2024-11-18 21:42:00 +0100

    Added changes in messages

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -3,7 +3,10 @@ import { Message } from '../Message/Message'
 
 export const Messages = () => {
   return (
-    <div>
+    <div className='flex flex-col gap-4 p-2' >
+      <Message  />
+      <Message  />
+      <Message  />
       <Message  />
     </div>
   )
