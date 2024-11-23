8d3c70e50015dfa4c60b2a5f78844153548e8516
1Bitif <aajliabdellatif@gmail.com>
2024-11-23 21:48:04 +0100

    Fix bug

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,12 +5,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBnIYWvJZYUiWvYt3Ei_cFVeR2qolrBoBQ",
-    authDomain: "app-chat-af595.firebaseapp.com",
-    projectId: "app-chat-af595",
-    storageBucket: "app-chat-af595.firebasestorage.app",
-    messagingSenderId: "936809093064",
-    appId: "1:936809093064:web:f44ce1ae35eb04897412fd",
-    measurementId: "G-J2VNN1D5DL"
+  authDomain: "app-chat-af595.firebaseapp.com",
+  projectId: "app-chat-af595",
+  storageBucket: "app-chat-af595.firebasestorage.app",
+  messagingSenderId: "936809093064",
+  appId: "1:936809093064:web:f44ce1ae35eb04897412fd",
+  measurementId: "G-J2VNN1D5DL"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
